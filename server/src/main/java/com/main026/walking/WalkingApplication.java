805dc77e42a82c8b8afd05f6effb3c48f9d20f23
KDK <liean17@gmail.com>
2022-09-15 09:47:04 +0900

    [FEAT] 서버 프로젝트 생성, 기본 의존성 추가

---
+++ server/src/main/java/com/main026/walking/WalkingApplication.java
@@ -0,0 +1,13 @@
+package com.main026.walking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkingApplication.class, args);
+	}
+
+}
