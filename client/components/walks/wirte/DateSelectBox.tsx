ed5049569698604824227250a05ba8e07ae757cf
JeonIJin <pongpong0121@kakao.com>
2022-09-15 18:48:36 +0900

    [FEAT] 산책 모임 만들기 페이지 (#5)
    * [FEAT] react-hook-form 설치
    
    * [FEAT] 산책 모임 만들기 페이지

---
+++ client/components/walks/wirte/DateSelectBox.tsx
@@ -0,0 +1,10 @@
+import TimeSelectBox from './TimeSelectBox';
+
+export default function DateSelectBox() {
+  return (
+    <>
+      <div>DateSelectBox</div>
+      <TimeSelectBox />
+    </>
+  );
+}
---
+++ client/components/walks/wirte/DaySelectBox.tsx
@@ -0,0 +1,10 @@
+import TimeSelectBox from './TimeSelectBox';
+
+export default function DaySelectBox() {
+  return (
+    <>
+      <div>DaySelectBox</div>
+      <TimeSelectBox />
+    </>
+  );
+}
