9bb5dcc3a316ccf496d50769cd8ccfba540e6030
JeonIJin <pongpong0121@kakao.com>
2022-09-16 13:06:15 +0900

    [FEAT] 날짜 선택 컴포넌트 (#7)
    * [FEAT] 컴포넌트 분리
    
    * [FEAT] click to edit 기능 제거 및 값에 따라 버튼 비활성화 기능 추가
    
    * [FEAT] react-dates 설치
    
    * [FEAT] react-dates 제거
    
    * [FEAT] react-datepicker 설치
    
    * [FEAT] 날짜 선택 컴포넌트

--- client/pages/walks/write.tsx
+++ client/pages/walks/write.tsx
@@ -4,15 +4,19 @@ import { useForm } from 'react-hook-form';
 import TabTitle from '../../components/TabTitle';
 import DateSelectBox from '../../components/walks/wirte/DateSelectBox';
 import DaySelectBox from '../../components/walks/wirte/DaySelectBox';
+import DescriptionInput from '../../components/walks/wirte/DescriptionInput';
+import NameInput from '../../components/walks/wirte/NameInput';
 import PersonCountInput from '../../components/walks/wirte/PersonCountInput';
+import PlaceInput from '../../components/walks/wirte/PlaceInput';
 import { WalksMoim } from '../../models/WalksMoim';
-import { Theme } from '../../styles/Theme';
+import 'react-datepicker/dist/react-datepicker.css';
 
 export default function Write() {
   const {
     register,
     handleSubmit,
     control,
+    setFocus,
     formState: { errors },
   } = useForm<WalksMoim>({
     mode: 'onChange',
@@ -36,49 +40,19 @@ export default function Write() {
           <ul>
             <li>
               <label htmlFor="moim-name">모임의 이름을 지어주세요.</label>
-              <input
-                type="text"
-                id="moim-name"
-                {...register('name', {
-                  validate: {
-                    empty: (v) =>
-                      (v != null && v !== '') || '모임의 이름을 입력해주세요.',
-                  },
-                })}
+              <NameInput
+                register={register}
+                errors={errors}
+                setFocus={setFocus}
               />
-              {errors.name ? <p>{errors.name.message}</p> : <p></p>}
             </li>
             <li>
               <label htmlFor="moim-place">자세한 모임 장소를 알려주세요.</label>
-              <input
-                type="text"
-                id="moim-place"
-                {...register('place', {
-                  validate: {
-                    empty: (v) =>
-                      (v != null && v !== '') || '모임의 장소를 입력해주세요.',
-                  },
-                })}
-              />
-              {errors.place ? <p>{errors.place.message}</p> : <p></p>}
+              <PlaceInput register={register} errors={errors} />
             </li>
             <li>
               <label htmlFor="moim-description">어떤 산책을 하고 싶나요?</label>
-              <textarea
-                id="moim-description"
-                placeholder="저희 강쥐가 낯을 많이 가려서 비슷하게 순둥하게 산책할 친구 찾아요!"
-                {...register('description', {
-                  validate: {
-                    empty: (v) =>
-                      (v != null && v !== '') || '내용을 입력해주세요.',
-                  },
-                })}
-              />
-              {errors.description ? (
-                <p>{errors.description.message}</p>
-              ) : (
-                <p></p>
-              )}
+              <DescriptionInput register={register} errors={errors} />
             </li>
             <li>
               <label htmlFor="moim-personCount">정원을 정해주세요</label>
@@ -86,8 +60,6 @@ export default function Write() {
             </li>
             <li>
               <label htmlFor="moim-plan">모임 날짜를 정해주세요.</label>
-            </li>
-            <li>
               <button
                 type="button"
                 className={plan === 'day' ? 'active' : ''}
@@ -103,7 +75,13 @@ export default function Write() {
                 날짜로 선택하기
               </button>
             </li>
-            <li>{plan === 'day' ? <DaySelectBox /> : <DateSelectBox />}</li>
+            <li>
+              {plan === 'day' ? (
+                <DaySelectBox />
+              ) : (
+                <DateSelectBox control={control} />
+              )}
+            </li>
             <li>
               <button
                 type="button"
@@ -122,12 +100,14 @@ export default function Write() {
 }
 
 const moimFormContainer = css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
   max-width: 800px;
   border: 1px solid;
   margin: 0 auto;
+
+  h1 {
+    margin: 38px;
+    text-align: start;
+  }
 
   form ul li {
     margin-top: 20px;
@@ -147,29 +127,7 @@ const moimFormContainer = css`
       display: block;
       margin-bottom: 20px;
       font-weight: 600;
-    }
-
-    input {
-      width: 100%;
-      padding: 10px;
-      background-color: #f7f7f5;
-      border: 1px solid #f7f7f5;
-      border-radius: 15px;
-    }
-
-    textarea {
-      width: 100%;
-      height: 200px;
-      padding: 10px;
-      background-color: ${Theme.inputBgColor};
-      border: 1px solid ${Theme.inputBgColor};
-      border-radius: 15px;
-    }
-
-    p {
-      height: 17px;
-      color: ${Theme.errorMessagesColor};
-      font-size: 12px;
+      font-size: 1.1rem;
     }
   }
 `;
--- client/styles/Theme.ts
+++ client/styles/Theme.ts
@@ -1,4 +1,6 @@
 export const Theme = {
+  mainColor: '#DC602A',
   inputBgColor: '#f7f7f5',
   errorMessagesColor: '#b90000',
+  disableColor: '#9b9b9b',
 };
