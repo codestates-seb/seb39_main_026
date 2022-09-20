4555a174cfe221382fad71cb79fcb87ab9107e53
KDK <liean17@gmail.com>
2022-09-20 15:00:26 +0900

    [ADD] 모임조회시 회원의 펫 정보 포함

--- server/src/main/java/com/main026/walking/community/controller/CommunityController.java
+++ server/src/main/java/com/main026/walking/community/controller/CommunityController.java
@@ -4,6 +4,10 @@ import com.main026.walking.community.dto.CommunityDto;
 import com.main026.walking.community.entity.Community;
 import com.main026.walking.community.mapper.CommunityMapper;
 import com.main026.walking.community.service.CommunityService;
+import com.main026.walking.member.entity.Member;
+import com.main026.walking.member.repository.MemberRepository;
+import com.main026.walking.pet.dto.PetDto;
+import com.main026.walking.pet.entity.Pet;
 import com.main026.walking.util.response.MultiResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,65 +16,86 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/community")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*",allowedHeaders = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CommunityController {
-  private final CommunityService communityService;
-  private final CommunityMapper communityMapper;
+    private final CommunityService communityService;
+    private final CommunityMapper communityMapper;
 
-//  Create
-  @PostMapping
-  public ResponseEntity postCommunity(@RequestBody CommunityDto.Post postDto){
+    //시큐리티 도입하면 삭제
+    private final MemberRepository memberRepository;
 
-    //TODO 유저 수정 필요
-    Community createdCommunity = communityService.createCommunity(postDto);
+    //  Create
+    @PostMapping
+    public ResponseEntity postCommunity(@RequestBody CommunityDto.Post postDto) {
 
-    return new ResponseEntity(communityMapper.entityToDtoResponse(createdCommunity), HttpStatus.CREATED);
-  }
+        //TODO 유저 수정 필요
+        Community createdCommunity = communityService.createCommunity(postDto);
 
-//  Read
-  @GetMapping("/{community-id}")
-  public ResponseEntity getCommunity(@PathVariable("community-id") long communityId){
-    Community community = communityService.findCommunity(communityId);
-    community.countView();
+        return new ResponseEntity(communityMapper.entityToDtoResponse(createdCommunity), HttpStatus.CREATED);
+    }
 
-    return new ResponseEntity(communityMapper.entityToDtoResponse(community), HttpStatus.OK);
-  }
+    //  Read
+    // 현재 로그인한 회원의 정보(강아지정보)를 전달해줘야한다.
+    @GetMapping("/{community-id}")
+    public ResponseEntity getCommunity(@PathVariable("community-id") long communityId) {
+        Community community = communityService.findCommunity(communityId);
+        community.countView();
+
+        //임시 - 현재로그인한 회원의 정보
+        Member kimcoding = memberRepository.findById(1L).orElseThrow();
+        List<PetDto.compactResponse> petList = kimcoding.getPetList().stream().map(pet -> new PetDto.compactResponse(pet)).collect(Collectors.toList());
+
+        CommunityDto.Response response = communityMapper.entityToDtoResponse(community);
+        response.setPetList(petList);
+
+        return new ResponseEntity(response, HttpStatus.OK);
+    }
+
+    // TODO 커뮤니티 참여에서 회원 데이터(PrincipalDetails) 필요
+    @PostMapping("/{community-id}")
+    public ResponseEntity joinCommunity(@PathVariable("community-id") long communityId) {
+        //찾은 멤버 데이터 - 현재 김코딩
+        Member kimcoding = memberRepository.findById(1L).orElseThrow();
+
+        return null;
+    }
 
 
-  @GetMapping
-  public ResponseEntity getCommunities(
-    @RequestParam(value = "page", defaultValue = "1") int page,
-    @RequestParam(value = "size", defaultValue = "10") int size ) {
+    @GetMapping
+    public ResponseEntity getCommunities(
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
 
-    Page<Community> communityPage = communityService.findCommunities(page - 1, size);
+        Page<Community> communityPage = communityService.findCommunities(page - 1, size);
 
-    List<Community> communities = communityPage.getContent();
+        List<Community> communities = communityPage.getContent();
 
-    //Todo 정보를 보여주는 dto 에 대한 고민
-    return new ResponseEntity(new MultiResponseDto<>(communityMapper.multiEntityToDtoInfo(communities), communityPage), HttpStatus.OK);
-  }
+        //Todo 정보를 보여주는 dto 에 대한 고민
+        return new ResponseEntity(new MultiResponseDto<>(communityMapper.multiEntityToDtoInfo(communities), communityPage), HttpStatus.OK);
+    }
 
-//  Update
-  @PatchMapping("{community-id}")
-  public ResponseEntity patchCommunity(
-    @PathVariable("community-id") long communityId,
-    @RequestBody CommunityDto.Patch dto ){
+    //  Update
+    @PatchMapping("{community-id}")
+    public ResponseEntity patchCommunity(
+            @PathVariable("community-id") long communityId,
+            @RequestBody CommunityDto.Patch dto) {
 
-    communityService.updateCommunity(communityId, dto);
-    Community community = communityService.findCommunity(communityId);
+        communityService.updateCommunity(communityId, dto);
+        Community community = communityService.findCommunity(communityId);
 
-    return new ResponseEntity<>(communityMapper.entityToDtoResponse(community), HttpStatus.OK);
-  }
+        return new ResponseEntity<>(communityMapper.entityToDtoResponse(community), HttpStatus.OK);
+    }
 
-//  Delete
-  @DeleteMapping("{community-id}")
-  public ResponseEntity deleteCommunity(
-    @PathVariable("community-id") long communityId){
-    communityService.deleteCommunity(communityId);
-    return new ResponseEntity(HttpStatus.NO_CONTENT);
-  }
+    //  Delete
+    @DeleteMapping("{community-id}")
+    public ResponseEntity deleteCommunity(
+            @PathVariable("community-id") long communityId) {
+        communityService.deleteCommunity(communityId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
 }
--- server/src/main/java/com/main026/walking/community/dto/CommunityDto.java
+++ server/src/main/java/com/main026/walking/community/dto/CommunityDto.java
@@ -17,78 +17,93 @@ import java.util.List;
 
 public class CommunityDto {
 
-  @Getter
-  @AllArgsConstructor @NoArgsConstructor
-  public static class Post{
-    private String name;
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Post {
+        private String name;
 
-    private List<MultipartFile> Images = new ArrayList<>();
+        private List<MultipartFile> Images = new ArrayList<>();
 
-    /**
-     * 시,구,동을 받아야하는데 나눠서 받는 방법 외에 또 있을까?
-     * 영어명보다 좀 더 직관적이려고 한글발음을 썼는데 좀 아닌것같기도하다.
-     */
-    private String si;
-    private String gu;
-    private String dong;
+        /**
+         * 시,구,동을 받아야하는데 나눠서 받는 방법 외에 또 있을까?
+         * 영어명보다 좀 더 직관적이려고 한글발음을 썼는데 좀 아닌것같기도하다.
+         */
+        private String si;
+        private String gu;
+        private String dong;
 
-    private String body;
-    private Integer capacity;
+        private String body;
+        private Integer capacity;
 
-    //날짜로 받는 경우
-    private String date;
-    //요일로 받는 경우
-    private String[] dates;
+        //날짜로 받는 경우
+        private String date;
+        //요일로 받는 경우
+        private String[] dates;
 
-    private String time;
+        private String time;
 
-    private LocalDateTime createdAt = LocalDateTime.now();
-  }
+        private LocalDateTime createdAt = LocalDateTime.now();
+    }
 
-  @Getter
-  @AllArgsConstructor @NoArgsConstructor
-  public static class Patch{
-    private String name;
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Patch {
+        private String name;
 
-    private String si;
-    private String gu;
-    private String dong;
+        private String si;
+        private String gu;
+        private String dong;
 
-    //날짜로 받는 경우
-    private String date;
-    //요일로 받는 경우
-    private String[] dates;
+        //날짜로 받는 경우
+        private String date;
+        //요일로 받는 경우
+        private String[] dates;
 
-    private String time;
+        private String time;
 
-    private String body;
-    private Integer capacity;
-    private String imgUrl;
-  }
+        private String body;
+        private Integer capacity;
+        private String imgUrl;
+    }
 
-  @Getter
-  @AllArgsConstructor @NoArgsConstructor
-  @Builder
-  public static class Response{
-    private Long communityId;
-    private String name;
-    private Address address;
-    private String body;
-    private Integer capacity;
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Response {
+        private Long communityId;
+        private String name;
+        private Address address;
+        private String body;
+        private Integer capacity;
 
-    private List<String> dayInfo;
-    private String dateInfo;
-    private String time;
+        private List<String> dayInfo;
+        private String dateInfo;
+        private String time;
 
-    //private List<String> weeks;
-    private MemberDto.Response member;
-    private List<String> imgUrls;
-    private List<PetDto.Response> pets;
-    private List<CommentDto.Response> comments;
-    private List<NoticeDto.Response> notices;
-    private Integer participant;
-    private LocalDateTime createdAt;
-    private Long viewed;
-    private Long liked;
-  }
+        //private List<String> weeks;
+        private MemberDto.Response member;
+        private List<String> imgUrls;
+        private List<PetDto.Response> pets;
+        private List<CommentDto.Response> comments;
+        private List<NoticeDto.Response> notices;
+        private Integer participant;
+        private LocalDateTime createdAt;
+        private Long viewed;
+        private Long liked;
+
+
+        /**Todo 커뮤니티 응답데이터에 세션회원의 정보를 담는게 맞는가?
+         * 응답에 로그인된 회원의 정보가 담기는게 맞을까?
+         * 로그인하지 않은 상태에서도 null 값이지만 정보가 들어가는 불필요함이 생긴다.
+         */
+
+        private List<PetDto.compactResponse> memberPetList;
+
+        public void setPetList(List<PetDto.compactResponse> petList){
+            this.memberPetList = petList;
+        }
+    }
 }
