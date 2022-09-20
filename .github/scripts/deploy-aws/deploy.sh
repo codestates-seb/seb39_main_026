b0d93723feca96930c5f17b1dbe58efa7ea73cd3
devEdok13 <dev.edok13@gmail.com>
2022-09-21 08:34:19 +0900

    [FIX] Deploy.sh 수정

--- .github/scripts/deploy-aws/deploy.sh
+++ .github/scripts/deploy-aws/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ubuntu/action/build/libs/walking-0.0.1-SNAPSHOT.jar)
+BUILD_JAR=$(ls /home/ubuntu/action/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 
 echo "> 현재 시간: $(date)" >> /home/ubuntu/action/deploy.log
@@ -10,19 +10,18 @@ echo "> build 파일 복사" >> /home/ubuntu/action/deploy.log
 DEPLOY_PATH=/home/ubuntu/action/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ubuntu/action/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "> 8080 PORT 에서 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(lsof -ti tcp:8080)
 
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ubuntu/action/deploy.log
 else
   echo "> kill -15 $CURRENT_PID" >> /home/ubuntu/action/deploy.log
-  sudo kill -15 $CURRENT_PID
+  kill -15 $CURRENT_PID
   sleep 5
 fi
 
-
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-sudo nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ubuntu/deploy.log 2>/home/ubuntu/action/deploy_err.log &
