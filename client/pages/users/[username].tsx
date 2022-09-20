dd0f23b1b508885becd714d796d2d33419ec533d
tai song <pseudopool@gmail.com>
2022-09-20 19:40:23 +0900

    [CHORE] 유저 프로필 컴포넌트 분리

--- client/pages/users/[username].tsx
+++ client/pages/users/[username].tsx
@@ -1,12 +1,11 @@
 import { css } from '@emotion/react';
 import { Icon } from '@iconify/react';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import TabTitle from '../../components/TabTitle';
+import PetInfo from '../../components/users/PetInfo';
+import UserInfo from '../../components/users/UserInfo';
 import { useGetUsersQuery } from '../../components/users/UsersQuery';
-import { MyPets } from '../../models/MyPets';
-import { WalkDefault } from '../../models/WalkDefault';
+import WalksInfo from '../../components/users/WalksInfo';
 import { Theme } from '../../styles/Theme';
 
 export default function User() {
@@ -14,58 +13,21 @@ export default function User() {
   const username = router.query.username as string;
   const UserData = useGetUsersQuery();
   const user = css`
+    margin: 15vw 20vw;
     display: flex;
     flex-direction: column;
-    align-items: center;
-    .userInfo {
-      margin-top: 2rem;
-      display: flex;
-      .img {
-        height: 75px;
-        width: 75px;
-        border-radius: 50%;
-      }
-      .username {
-        margin-left: 1rem;
-        font-size: 22px;
-      }
-    }
-    .petInfo {
-      margin-top: 1rem;
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      .petImg {
-        height: 50px;
-        width: 50px;
-        border-radius: 50%;
-      }
-      .petName {
-        font-size: 10px;
-        font-weight: 600;
-      }
-    }
     .walks {
-      margin-top: 1rem;
-      p {
-        display: flex;
-        align-items: center;
-        font-size: 18px;
-        font-weight: 500;
-        padding: 1rem;
-        border-bottom: 1px solid ${Theme.divisionLineColor};
-        .icon {
-          font-size: 1.5rem;
-        }
-        span {
-          margin-left: 0.5rem;
-          font-weight: 600;
-          color: ${Theme.mainColor};
-        }
+      display: flex;
+      padding: 1rem 0;
+      border-bottom: 1px solid ${Theme.divisionLineColor};
+      font-weight: 500;
+      .icon {
+        font-size: 1.7rem;
       }
-      a {
-        text-decoration: none;
-        color: black;
+      span {
+        font-weight: 600;
+        color: ${Theme.mainColor};
+        margin: 0 0.2rem 0rem 0.8rem;
       }
     }
   `;
@@ -73,52 +35,13 @@ export default function User() {
   return (
     <section css={user}>
       <TabTitle prefix={username} />
-      <div className="userInfo">
-        <div className="img">
-          <Image
-            alt={`${username}'s profile`}
-            src={UserData.imgUrl}
-            width="75px"
-            height="75px"
-            className="img"
-          />
-        </div>
-        <p className="username">{username}</p>
-      </div>
-      <div className="petWrapper">
-        {UserData?.petResponseDtoList?.map((pet: MyPets) => {
-          return (
-            <div className="petInfo" key={pet.id}>
-              <div className="petImg">
-                <Image
-                  src={pet.imgUrl}
-                  alt={`${pet.petName}'s picture`}
-                  height="50px"
-                  width="50px"
-                  className="petImg"
-                />
-              </div>
-              <p className="petName">{pet.petName}</p>
-            </div>
-          );
-        })}
-      </div>
+      <UserInfo data={UserData} />
+      <PetInfo pets={UserData?.petResponseDtoList} />
       <div className="walks">
-        <p>
-          <Icon icon="fluent-emoji-flat:paw-prints" className="icon" />
-          <span> {username}</span> 님의 산책 모임
-        </p>
-        {UserData?.community?.map((community: WalkDefault) => {
-          return (
-            <Link
-              href={`/walks/${community.communityId}`}
-              key={community.communityId}
-            >
-              <a>{community.title}</a>
-            </Link>
-          );
-        })}
+        <Icon icon="fluent-emoji-flat:paw-prints" className="icon" />
+        <span>{username}</span>님의 산책 모임
       </div>
+      <WalksInfo walks={UserData?.community} />
     </section>
   );
 }
