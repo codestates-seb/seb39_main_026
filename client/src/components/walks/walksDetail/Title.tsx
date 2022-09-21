894ad044cefee75f65c16c3c99e4d7e1ae7b14d1
pongdang <pongpong0121@kakao.com>
2022-09-21 11:31:07 +0900

    [FEAT] 강아지 정보를 볼 수 있는 모달 적용

--- client/src/components/walks/walksDetail/Title.tsx
+++ client/src/components/walks/walksDetail/Title.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { css } from '@emotion/react';
+import { Dispatch, SetStateAction } from 'react';
 import { WalkDetail } from '../../../models/WalkDefault';
 import { Theme } from '../../../styles/Theme';
 import MoimState from '../../MoimState';
@@ -11,6 +12,12 @@ const titleContainer = css`
   h1 {
     font-size: 1.6rem;
     letter-spacing: 0.4px;
+    @media screen and (max-width: 525px) {
+      font-size: 1.3rem;
+    }
+    @media screen and (max-width: 324px) {
+      font-size: 1rem;
+    }
   }
 `;
 
@@ -47,7 +54,15 @@ const postOwnerContainer = css`
   }
 `;
 
-export default function Title({ walksData }: { walksData: WalkDetail }) {
+export default function Title({
+  walksData,
+  setIsDogInfoModalOpen,
+  getPetId,
+}: {
+  walksData: WalkDetail;
+  setIsDogInfoModalOpen: Dispatch<SetStateAction<boolean>>;
+  getPetId: (petId: string) => void;
+}) {
   function getMoimState() {
     // 참여자 < 모집인원 && 시간이 지나지 않음 -> 모집중
     if (
@@ -79,8 +94,12 @@ export default function Title({ walksData }: { walksData: WalkDetail }) {
             <img
               src="/main_image.jpg"
               alt={`작성자 ${walksData.representMember}사진`}
+              onClick={() => {
+                setIsDogInfoModalOpen(true);
+                getPetId('1');
+              }}
             />
-            {walksData.pets.map((pet) =>
+            {walksData.pets?.map((pet) =>
               pet.username === walksData.representMember ? (
                 <p key={pet.petName}>{pet.petName}</p>
               ) : null
