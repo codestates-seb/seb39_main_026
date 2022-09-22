f2964c3a94edf956ac4b8cec0e7023acb29312e3
devEdok13 <dev.edok13@gmail.com>
2022-09-22 11:22:32 +0900

    [FIX] Log 출력 옵션 변경2

--- .github/scripts/deploy-aws/deploy.sh
+++ .github/scripts/deploy-aws/deploy.sh
@@ -24,6 +24,4 @@ fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 echo "> DEPLOY_JAR 배포"    >> /home/ubuntu/action/deploy.log
-cd $DEPLOY_PATH
-nohup java -jar $JAR_NAME  --logging.file.path=$DEPLOY_PATH --logging.level.org.hibernate.SQL=DEBUG
->> $DEPLOY_PATH/deploy.log 2>$DEPLOY_PATH/deploy_err.log &
+nohup java -jar $DEPLOY_JAR --logging.level.org.hibernate.SQL=DEBUG >> $DEPLOY_PATH/spring.log 2>$DEPLOY_PATH/deploy_err.log &
