3e9af2494fb12259f31eec193c19b8938db4dcdc
KDK <liean17@gmail.com>
2022-09-23 15:48:51 +0900

    [FIX] 예외 처리 수정

--- server/src/main/java/com/main026/walking/exception/BusinessLogicException.java
+++ server/src/main/java/com/main026/walking/exception/BusinessLogicException.java
@@ -8,7 +8,7 @@ public class BusinessLogicException extends RuntimeException{
     private ExceptionCode exceptionCode;
 
     public BusinessLogicException(ExceptionCode exceptionCode) {
-        super(exceptionCode.getMessage());
+        super(exceptionCode.getError());
         this.exceptionCode = exceptionCode;
     }
 }
