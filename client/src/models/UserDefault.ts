08bd0c267473294bbb6ca42cf03b1c1a33a63943
pongdang <pongpong0121@kakao.com>
2022-09-26 15:23:51 +0900

    [CHORE] 주석 및 console.log 제거

--- client/src/models/UserDefault.ts
+++ client/src/models/UserDefault.ts
@@ -1,9 +1,6 @@
 export interface UserDefault {
   id: number;
   username: string;
-  // NOTE: UserDefault 인터페이스가 useGetUsersQuery 응답 타입으로 쓰이는데,
-  // password는 응답값으로 내려오지 않으니까 지우는게 맞지 않을까요?
-  password: string;
   imgUrl: string;
   dong: string;
   gu: string;
