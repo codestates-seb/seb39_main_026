eeecd0bc7d1635cbc352e0fe548703d8ba97f9d1
JeonIJin <pongpong0121@kakao.com>
2022-09-27 16:48:05 +0900

    [FEAT] 모임 공지사항 API 연결 (#80)
    * [CHORE] 버튼 컴포넌트 css 수정
    
    * [FEAT] 모임 공지사항 API 연결

--- client/src/pages/walks/[walkId]/notice.tsx
+++ client/src/pages/walks/[walkId]/notice.tsx
@@ -1,17 +1,28 @@
-import { useRouter } from 'next/router';
+import { GetServerSideProps } from 'next';
 import TabTitle from '../../../components/TabTitle';
 import DetailLayout from '../../../components/walks/walksDetail/DetailLayout';
 import MoimNotice from '../../../components/walks/walksDetail/MoimNotice';
+import { useWalksDetailQuery } from '../../../hooks/WalksDetailQuery';
 
-export default function Notice() {
-  const router = useRouter();
-  const walkId = router.query.walkId as string;
+export default function Notice({ walkId }: { walkId: string }) {
+  const walkDetail = useWalksDetailQuery(walkId);
+
   return (
     <>
       <TabTitle prefix={walkId} />
       <DetailLayout walkId={walkId}>
-        <MoimNotice />
+        <MoimNotice walkDetail={walkDetail} />
       </DetailLayout>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { query } = context;
+  const { walkId } = query;
+  return {
+    props: {
+      walkId,
+    },
+  };
+};
