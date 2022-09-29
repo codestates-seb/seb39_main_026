7b3195eb99bd4b30e30473e80654fa980f3424fb
pongdang <pongpong0121@kakao.com>
2022-09-29 10:25:40 +0900

    [CHORE] 중복된 코드 수정

--- client/src/apis/api.ts
+++ client/src/apis/api.ts
@@ -2,7 +2,6 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const API = {
   WALKS: `${BASE_URL}/community`,
-  COMMUNITYDETAIL: `${BASE_URL}/community`,
   USERS: `${BASE_URL}/members`,
   PETS: `${BASE_URL}/pets`,
   Login: `${BASE_URL}/members/login`,
