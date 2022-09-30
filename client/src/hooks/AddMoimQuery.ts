ac5355b4ae6f9759f4a99b9f50809a0410f969f2
JeonIJin <pongpong0121@kakao.com>
2022-09-30 23:01:08 +0900

    [FEAT] write 페이지에서 이미지 추가하기 (#109)

--- client/src/hooks/AddMoimQuery.ts
+++ client/src/hooks/AddMoimQuery.ts
@@ -3,10 +3,36 @@ import { dateToHMM, dateToYYYYMMDD } from '../../util/transformDate';
 import { API } from '../apis/api';
 import { WalksMoimOneDay, WalksMoimEveryWeek } from '../models/WalksMoim';
 
+export function usePostMoimImage() {
+  const handlePostMoimImage = async (files: File[]) => {
+    const formData = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      formData.append('imgFile', files[i]);
+    }
+
+    const res = await axios.post(`${API.WALKS}/post/image`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        authorization: localStorage.getItem('accessToken') || '',
+        refresh_token: localStorage.getItem('refreshToken') || '',
+      },
+    });
+
+    console.log('PostMoimImage', res);
+    return res.data;
+  };
+
+  return { handlePostMoimImage } as const;
+}
+
 export function useAddOneDayMoim() {
-  const handleAddOneDayMoim = async (data: WalksMoimOneDay) => {
+  const handleAddOneDayMoim = async (
+    data: WalksMoimOneDay,
+    moimImg: string[]
+  ) => {
     const res = await axios.post(
-      API.WALKS,
+      `${API.WALKS}/post`,
       {
         name: data.title,
         body: data.description,
@@ -17,7 +43,7 @@ export function useAddOneDayMoim() {
         si: data.si,
         gu: data.gu,
         dong: data.dong,
-        // images: data.images,
+        imgUrls: [...moimImg],
       },
       {
         headers: {
@@ -27,6 +53,23 @@ export function useAddOneDayMoim() {
       }
     );
 
+    console.log(
+      {
+        name: data.title,
+        body: data.description,
+        capacity: data.personCount,
+        place: data.place,
+        date: dateToYYYYMMDD(data.plannedDate),
+        time: dateToHMM(data.plannedTime),
+        si: data.si,
+        gu: data.gu,
+        dong: data.dong,
+        imgUrls: [...moimImg],
+      },
+      'moim'
+    );
+    console.log(res, 'onedayMoimRes');
+
     return res;
   };
 
@@ -34,9 +77,33 @@ export function useAddOneDayMoim() {
 }
 
 export function useAddEveryWeekMoim() {
-  const handleAddEveryWeekMoim = async (data: WalksMoimEveryWeek) => {
+  const handleAddEveryWeekMoim = async (
+    data: WalksMoimEveryWeek,
+    moimImg: string[]
+  ) => {
     const res = await axios.post(
-      API.WALKS,
+      `${API.WALKS}/post`,
+      {
+        name: data.title,
+        body: data.description,
+        capacity: data.personCount,
+        place: data.place,
+        dates: data.plannedDates,
+        time: dateToHMM(data.plannedTime),
+        si: data.si,
+        gu: data.gu,
+        dong: data.dong,
+        imgUrls: [...moimImg],
+      },
+      {
+        headers: {
+          authorization: localStorage.getItem('accessToken') || '',
+          refresh_token: localStorage.getItem('refreshToken') || '',
+        },
+      }
+    );
+
+    console.log(
       {
         name: data.title,
         body: data.description,
@@ -48,14 +115,12 @@ export function useAddEveryWeekMoim() {
         gu: data.gu,
         dong: data.dong,
         // images: data.images,
+        imgUrls: [...moimImg],
       },
-      {
-        headers: {
-          authorization: localStorage.getItem('accessToken') || '',
-          refresh_token: localStorage.getItem('refreshToken') || '',
-        },
-      }
+      'moim'
     );
+    console.log(res, 'everyWeekMoimRes');
+
     return res;
   };
 
