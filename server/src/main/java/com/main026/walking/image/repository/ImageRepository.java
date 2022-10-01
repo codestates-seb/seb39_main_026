d4dadf329ba57cb439542ba7f4a96a7563f37afe
devEdok13 <104124893+devEdok13@users.noreply.github.com>
2022-10-02 08:01:17 +0900

    [FIX] Community 이미지 관련 기능 수정 (#111)
    * [FIX] Community Default Image 적용 및 awsS3Service 메소드 일원화
    
    * [FIX] Community 등록시 이미지 처리 로직 수정 및 엔드포인트 컨벤션 통일
    
    * [FIX] Community Image Post 요청시 S3에 저장된 이름을 반환
    
    * [FIX] Community Dto Response 매핑시 AwsS3Service 이중매핑 수정
    
    * [FIX] Community Post시에 응답에 저장된 Image를 담을 수 있도록 수정

--- server/src/main/java/com/main026/walking/image/repository/ImageRepository.java
+++ server/src/main/java/com/main026/walking/image/repository/ImageRepository.java
@@ -4,9 +4,12 @@ import com.main026.walking.image.entity.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Long> {
   Optional<Image> findByStoreFilename(String filename);
+
+  List<Image> findByCommunityId(Long communityId);
 }
