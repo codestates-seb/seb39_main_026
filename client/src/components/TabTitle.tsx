f816aacada87e74bb636f3f8f05cb48619ab89b2
tai song <98731086+pseudopool@users.noreply.github.com>
2022-10-05 15:09:23 +0900

    [FEAT] 메타 태그 오픈 그래프 설정 (#139)
    * [CHORE] console.log 제거 및 url 정리
    
    * [CHORE] 공지사항 모달 컴포넌트 생성
    
    * [FIX] 강아지 정보 모달 나이 관련 코드 수정
    
    * [FEAT] 오픈 그래프 세팅

--- client/src/components/TabTitle.tsx
+++ client/src/components/TabTitle.tsx
@@ -10,6 +10,15 @@ export default function TabTitle({ prefix }: { prefix?: string }) {
   return (
     <Head>
       <title>{title}</title>
+      <meta property="og:title" content={title} />
+      <meta
+        property="og:description"
+        content="소심견도, 인싸견도 괜찮아! 성향대로 모이는 동네 기반 산책 모임 플랫폼"
+      />
+      <meta
+        property="og:image"
+        content="https://github.com/codestates-seb/seb39_main_026/blob/main/metaImage.png?raw=true"
+      />
     </Head>
   );
 }
