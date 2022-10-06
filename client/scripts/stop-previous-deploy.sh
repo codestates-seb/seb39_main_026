1219da412078116678d35b8ff564b9cd550377ee
pongdang <pongpong0121@kakao.com>
2022-10-06 16:32:01 +0900

    fix

--- client/scripts/stop-previous-deploy.sh
+++ client/scripts/stop-previous-deploy.sh
@@ -1,3 +1,3 @@
 # NOTE: 여러번 실행되면 서버에서 여러 번 빌드되면서 서버의 사용량이 급증합니다.
 # 따라서, 이전에 실행된 배포 프로세스를 종료합니다.
-ps -ef | grep "deploy.sh" | grep -v "grep" | awk '{print $2;}' | xargs kill -9 2> /dev/null || echo "no-process-to-kill"
+ps -ef | grep "next build" | grep -v "grep" | awk '{print $2;}' | xargs kill -9 2> /dev/null || echo "no-process-to-kill"
